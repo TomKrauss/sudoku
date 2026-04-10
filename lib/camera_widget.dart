49175e19659c38c375da85648c23c0d6c4e8216c
Tom Krauss <tom.krauss@gebit.de>
2026-04-10 10:27:54 +0200

    NO-ISSUE: started to implement Sudoku Scanning from Paper.

--- lib/camera_widget.dart
+++ lib/camera_widget.dart
@@ -9,8 +9,13 @@
 // THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
+import 'dart:io';
+
 import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:sudoku/model.dart';
 
 class CameraWidget extends StatefulWidget {
@@ -21,13 +26,23 @@ class CameraWidget extends StatefulWidget {
 }
 
 class _CameraWidgetState extends State<CameraWidget> {
+  final _orientations = {
+    DeviceOrientation.portraitUp: 0,
+    DeviceOrientation.landscapeLeft: 90,
+    DeviceOrientation.portraitDown: 180,
+    DeviceOrientation.landscapeRight: 270,
+  };
+  final _script = TextRecognitionScript.latin;
+  final _textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
   CameraController? cameraController;
+  CameraDescription? currentCamera;
   String? noCameraText = "Waiting for Camera to initialize";
 
   Future<void> initCameras() async {
     var cameras = await availableCameras();
     if (cameras.isNotEmpty) {
-      cameraController = CameraController(cameras[0], ResolutionPreset.max);
+      currentCamera = cameras[0];
+      cameraController = CameraController(currentCamera!, ResolutionPreset.max);
       await cameraController!
           .initialize()
           .then((_) {
@@ -67,13 +82,98 @@ class _CameraWidgetState extends State<CameraWidget> {
     initCameras();
   }
 
+  InputImage? _inputImageFromCameraImage(CameraImage image) {
+    if (cameraController == null) {
+      return null;
+    }
+    // get image rotation
+    // it is used in android to convert the InputImage from Dart to Java: https://github.com/flutter-ml/google_ml_kit_flutter/blob/master/packages/google_mlkit_commons/android/src/main/java/com/google_mlkit_commons/InputImageConverter.java
+    // `rotation` is not used in iOS to convert the InputImage from Dart to Obj-C: https://github.com/flutter-ml/google_ml_kit_flutter/blob/master/packages/google_mlkit_commons/ios/Classes/MLKVisionImage%2BFlutterPlugin.m
+    // in both platforms `rotation` and `camera.lensDirection` can be used to compensate `x` and `y` coordinates on a canvas: https://github.com/flutter-ml/google_ml_kit_flutter/blob/master/packages/example/lib/vision_detector_views/painters/coordinates_translator.dart
+    final camera = currentCamera!;
+    final sensorOrientation = camera.sensorOrientation;
+    // print(
+    //     'lensDirection: ${camera.lensDirection}, sensorOrientation: $sensorOrientation, ${_controller?.value.deviceOrientation} ${_controller?.value.lockedCaptureOrientation} ${_controller?.value.isCaptureOrientationLocked}');
+    InputImageRotation? rotation;
+    if (Platform.isIOS) {
+      rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
+    } else if (Platform.isAndroid) {
+      var rotationCompensation = _orientations[cameraController!.value.deviceOrientation];
+      if (rotationCompensation == null) {
+        return null;
+      }
+      if (camera.lensDirection == CameraLensDirection.front) {
+        // front-facing
+        rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
+      } else {
+        // back-facing
+        rotationCompensation =
+            (sensorOrientation - rotationCompensation + 360) % 360;
+      }
+      rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
+      // print('rotationCompensation: $rotationCompensation');
+    }
+    if (rotation == null) {
+      return null;
+    }
+    // print('final rotation: $rotation');
+
+    // get image format
+    final format = InputImageFormatValue.fromRawValue(image.format.raw);
+    // validate format depending on platform
+    // only supported formats:
+    // * nv21 for Android
+    // * bgra8888 for iOS
+    if (format == null ||
+        (Platform.isAndroid && format != InputImageFormat.nv21) ||
+        (Platform.isIOS && format != InputImageFormat.bgra8888)) {
+      return null;
+    }
+
+    // since format is constraint to nv21 or bgra8888, both only have one plane
+    if (image.planes.length != 1) {
+      return null;
+    }
+    final plane = image.planes.first;
+
+    // compose InputImage using bytes
+    return InputImage.fromBytes(
+      bytes: plane.bytes,
+      metadata: InputImageMetadata(
+        size: Size(image.width.toDouble(), image.height.toDouble()),
+        rotation: rotation, // used only in Android
+        format: format, // used only in iOS
+        bytesPerRow: plane.bytesPerRow, // used only in iOS
+      ),
+    );
+  }
+
+  void textDetected(List<TextBlock> text) {
+    for (final b in text) {
+      b.text;
+    }
+  }
+
   Future<void> _takePicture() async {
     var controller = cameraController;
     if (controller == null) {
       return;
     }
-    var file = await controller.takePicture();
-    await file.readAsBytes();
+    if (!controller.supportsImageStreaming()) {
+      final picture = await controller.takePicture();
+      final image = InputImage.fromFilePath(picture.path);
+      var text = await _textRecognizer.processImage(image);
+      textDetected(text.blocks);
+      return;
+    }
+    await controller.startImageStream((cameraImage) async {
+      var image = _inputImageFromCameraImage(cameraImage);
+      if (image != null) {
+        await controller.stopImageStream();
+        var text = await _textRecognizer.processImage(image);
+        textDetected(text.blocks);
+      }
+    });
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@
 // THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -459,6 +460,7 @@ class _SudokuBoardState extends State<SudokuBoard> {
           style: buttonStyle,
           child: Text("Save"),
         ),
+        if (Platform.isIOS || Platform.isAndroid)
         ElevatedButton(
           onPressed: scanGame,
           style: buttonStyle,
